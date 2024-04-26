c1df69c6e363948fda1f65971d1f62bf85f14e6b
b4udineshkannan <b4udineshkannan@gmail.com>
2024-04-27 03:52:37 +0530

    first commit

--- src/app/Navbar/Navbar.jsx
+++ src/app/Navbar/Navbar.jsx
@@ -106,7 +106,7 @@ const Navbar = () => {
                     alt='cv' 
                     width={100} 
                     height={100} 
-                    className='w-[50px] h-[auto] rounded-[100%] '
+                    className='w-[50px] h-[auto] rounded-[100%] bg-[#000] rounded-full'
                     />
                     </a>
                     </div>
