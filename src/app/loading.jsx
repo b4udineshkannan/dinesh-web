769d385871824f3f3fecd7969d5aaefc44e97592
b4udineshkannan <b4udineshkannan@gmail.com>
2024-04-27 12:44:28 +0530

    test

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,10 +1,16 @@
 
+import Image from 'next/image'
 import React from 'react'
 
 const loading = () => {
   return (
     <>
    <div className='loaderCss'>
+   <Image  src={"/public/Intro/yalantis-interactive-404.gif"}
+                            alt='intro'
+                            width={450}
+                            height={206}
+                            className='h-screen DCimg'/>
     <h2>loading</h2></div>
     </>
   )
