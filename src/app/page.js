038f88ab38e9fcc17f1e2d4f56478387e9814e50
b4udineshkannan <b4udineshkannan@gmail.com>
2024-04-27 09:48:45 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -10,27 +10,16 @@ export default function Home() {
   return (
   <>
   <div className="main-wrapper">
-   <video  className="bg-video fixed top-0 bottom-0 object-cover h-[100vh] w-[100%] "
-                    autoPlay
-                    muted
-                    playsInline
-                    loop
-                    preload="auto"
-                >
-                    <source
-                        src="/Intro/new-comp.mp4"
-                        type="video/mp4"
-                    />
-                    
-                </video>
-                <div className="absolute top-0 left-0 right-0 bg-[#00000083]">
+   
+             
 
-                <div className="container mx-auto">
-  <Scroll/>
+               
+  
   <Navbar />
+  <Scroll/>
  <CustomComponent/>
-  </div>
-  </div>
+
+  
   </div>
   </>
   );
